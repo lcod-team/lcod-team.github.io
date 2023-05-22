755619d7ec531e2057db2d2c3ac6dc8dd9ca5c6d
Nicolas Albert <nicolas.albert@gmail.com>
2023-05-22 19:08:40 +0200

    set under construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to lcod</h1>
-<p>Visit <a href="https://github.com/lcod-team">github.com/lcod-team</a> to see the code</p>
+<div>
+	<img
+		alt="lcod-logo"
+		src="https://github.com/lcod-team/lcod-assets/raw/main/logo/lcod_color_border_anim.svg"
+	/>
+	<p>
+		Under construction, please visit<br />
+		<a href="https://github.com/lcod-team">github.com/lcod-team</a><br />
+		to see the project updates.
+	</p>
+</div>
+
+<style>
+	:global(body) {
+		background-color: #0d1117;
+		color: white;
+	}
+	a:any-link {
+		color: white;
+	}
+	div {
+		text-align: center;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+</style>
